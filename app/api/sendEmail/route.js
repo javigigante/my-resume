79c0cfa2aad4d16f02b616b10bcbada250bda00f
Javier Díaz Gigante <javi.gigante474@gmail.com>
2024-12-04 15:07:45 +0100

    Commit 04122024 15:05h

--- app/api/sendEmail/route.js
+++ app/api/sendEmail/route.js
@@ -1,41 +1,64 @@
-import emailjs from 'emailjs-com';
+import nodemailer from 'nodemailer';
 
-export async function POST(req) {
+require('dotenv').config();
+
+export async function POST(req) { 
+
   try {
     const { name, email, phone, message } = await req.json();
-
-    // Validar los campos
-    if (!name || !email || !message) {
-      return new Response(JSON.stringify({ message: 'All fields are required' }), {
-        status: 400,
+    
+    if ( !name || !email || !message ) {
+      return new Response( JSON.stringify({ message: 'All fields are required' }),
+      { 
+        status: 400, 
       });
-    }
-
-    // Lógica de envío de correo con EmailJS
-    const response = await emailjs.send(
-      process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID,
-      process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID,
-      {
-        from_name: name,
-        from_email: email,
-        from_phone: phone || 'No especificado',
-        message: message,
+    };
+    
+    const transporter = nodemailer.createTransport({
+      host: 'smtp.gmail.com', 
+      port: 465, 
+      secure: true, 
+      auth: {
+        user: process.env.NEXT_PUBLIC_EMAIL_USER, 
+        pass: process.env.NEXT_PUBLIC_EMAIL_PASS, 
       },
-      process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY
-    );
+      tls: {
+        family: 4,
+      }
+    });
+    
+    const mailFromAPIToVIP = {
+      from: '"JaviGigante" <javigigante.dev@gmail.com>', 
+      to: 'javigigante.dev@gmail.com', 
+      subject: `${name} quiere contactar contigo!`, 
+      text: `${name}, con email: ${email} y con tf: ${phone}, te ha mandado un mensaje desde tu web: \n\n${message}`,
+    };
 
-    console.log('Email sent successfully:', response);
-    return new Response(JSON.stringify({ message: 'Email sent successfully' }), {
+    const mailFromAPIToSender = {
+      from: '"JaviGigante" <javigigante.dev@gmail.com>', 
+      to: email, 
+      subject: '¡Gracias por tu mensaje!', 
+      text: `Hola ${name},\n\nGracias por contactarme. Esto es lo que has enviado:\n\n${message}\n\nTe responderé a la mayor brevedad.\n\nSaludos,\nJaviGigante`,
+    };
+    
+    await transporter.sendMail( mailFromAPIToSender );
+    await transporter.sendMail( mailFromAPIToVIP );
+
+    return new Response( JSON.stringify({ message: 'Email sent successfully' }), 
+    {
       status: 200,
     });
   } catch (error) {
     console.error('Error sending email:', error);
     return new Response(
       JSON.stringify({ message: 'Failed to send email', error: error.message }),
-      { status: 500 }
+      { 
+        status: 500, 
+      }
     );
   }
 }
+
 
   
   
