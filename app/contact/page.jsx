57ae2fa383d15af68bef764f1bd0e83e413552a4
Javier Díaz Gigante <javi.gigante474@gmail.com>
2024-12-05 14:05:15 +0100

    Commit 05122024 14:05h

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -5,8 +5,8 @@ import ContactForm from "@/components/forms/ContactForm"
 const Contact = () => {
 
     return (
-        <div className="w-full z-20 h-screen lg:landscape:h-screen sm:landscape:h-[calc(200vh-10px)] flex justify-center items-center">
-            <div className="w-11/12 gap-4 h-full pb-8 flex flex-col justify-center items-center">
+        <div className="w-full z-20 h-screen lg:landscape:h-screen md:landscape:h-[calc(200vh-10px)] sm:landscape:h-[calc(200vh-10px)] flex justify-center items-center">
+            <div className="w-11/12 gap-4 h-full lg:pt-0 pb-8 flex flex-col justify-center items-center">
                 <div className="w-full">
                     <h3 className="font-bold text-center md:text-2xl text-lg">¡Contáctame!</h3>
                 </div>
--- components/forms/ContactForm.jsx
+++ components/forms/ContactForm.jsx
@@ -4,22 +4,19 @@ import React, { useRef, useState } from "react";
 
 const ContactForm = () => {
   const form = useRef();
-  const [messageSent, setMessageSent] = useState(false);
-  const [isButtonDisabled, setButtonDisabled] = useState(false);
+  const [ messageSent, setMessageSent ] = useState(false);
+  const [ isButtonDisabled, setButtonDisabled ] = useState(false);
 
   const sendEmail = async (e) => {
-    e.preventDefault();
+    e.preventDefault();    
     
-    // Verificar los datos del formulario
     const data = {
-      name: e.target.name.value,    // Asegúrate de que los nombres coincidan
-      email: e.target.email.value,  // 'name' y 'email' deben coincidir con los nombres de los campos
+      name: e.target.name.value,    
+      email: e.target.email.value,  
       phone: e.target.phone.value,
       message: e.target.message.value,
-    };
-    
-    console.log("Datos enviados:", data);  // Verificar que los datos sean correctos
-    
+    };  
+        
     try {
       setButtonDisabled(true);
       
@@ -31,61 +28,62 @@ const ContactForm = () => {
 
       if (response.ok) {
         setMessageSent(true);
-        console.log("Email sent successfully!");
+        console.log( "Email sent successfully!" );
         form.current.reset();
       } else {
         const errorData = await response.json();
-        console.error("Error:", errorData.message);
-        alert("Failed to send the email. Please try again.");
+        console.error( "Error:", errorData.message );
+        alert( "Failed to send the email. Please try again." );
       }
     } catch (error) {
-      console.error("Unexpected error:", error);
-      alert("Something went wrong. Please try again.");
+      console.error( "Unexpected error:", error );
+      alert( "Something went wrong. Please try again." );
     } finally {
       setButtonDisabled(false);
-      setTimeout(() => setMessageSent(false), 5000);
+      setTimeout( () => setMessageSent(false), 5000 );
     }
   };
 
   return (
-    <div className="w-full max-w-md glass md:px-5 px-2 md:py-10 py-5 rounded-xl">
-      <form ref={form} onSubmit={sendEmail} className="w-full flex flex-col gap-5">
+    <div className="w-full max-w-md glass md:px-5 md:py-10 py-5 rounded-xl">
+      <form ref={ form } onSubmit={ sendEmail } className="w-full flex flex-col gap-5 items-center justify-center">
         <input
           required
           type="text"
-          name="name"      // Nombre de campo correcto
+          name="name"      
           placeholder="Tu nombre:"
           className="input-field md:text-lg text-sm w-full md:p-3 p-2 outline-none border-b-2 border-white bg-slate-500 rounded-xl"
         />
         <input
           required
           type="text"
-          name="email"     // Nombre de campo correcto
+          name="email"     
           placeholder="Tu email:"
           className="input-field md:text-lg text-sm w-full md:p-3 p-2 outline-none border-b-2 border-white bg-slate-500 rounded-xl"
         />
         <input
           type="tel"
-          name="phone"     // Nombre de campo correcto
+          name="phone"     
           placeholder="Tu teléfono:"
           className="input-field md:text-lg text-sm w-full md:p-3 p-2 outline-none border-b-2 border-white bg-slate-500 rounded-xl"
         />
         <textarea
           required
-          name="message"   // Nombre de campo correcto
-          rows={5}
+          name="message"   
+          rows={ 5 }
           placeholder="Escribe aquí lo que quieras decirme:"
           className="input-field md:text-lg text-sm w-full md:p-3 p-2 outline-none border-b-2 border-white bg-slate-500 rounded-xl"
         />
-        <button disabled={isButtonDisabled} className="md:w-full w-40 md:h-10 h-8 bg-slate-700 rounded-lg text-white md:text-lg text-sm transition-all duration-150 ease-in-out hover:bg-primarycolor border-2 border-primarycolor hover:text-white">
+        <button disabled={ isButtonDisabled } 
+          className="md:w-full w-40 md:h-10 h-8 bg-slate-700 rounded-lg text-white md:text-lg text-sm transition-all duration-150 ease-in-out hover:bg-primarycolor border-2 border-primarycolor hover:text-white items-center justify-center">
           Enviar
         </button>
       </form>
-      {messageSent && (
+      { messageSent && (
         <div className="py-2 text-center text-sm">
           <h4>Tu mensaje ha sido enviado.<br />¡Te contestaré a la mayor brevedad!</h4>
         </div>
-      )}
+      ) }
     </div>
   );
 };
